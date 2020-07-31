126e2ba8a75d2e83e1e767f5018983286fcf2b8a
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-31 14:53:24 +0530

    listviewDone

--- lib/shop_details.dart
+++ lib/shop_details.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'model/sugessted_model.dart';
+import 'widget/suggested_grid_item.dart';
+
 class ShopDetails extends StatelessWidget {
+  List<Suggested> _images = [
+    Suggested(
+        brand: 'Subway. YSK',
+        price: '117',
+        title: 'Cheesy Delux Burger',
+        image:
+            'https://purepng.com/public/uploads/medium/purepng.com-burgersandwhichfoodbreadcaviarburgerdelicious-21526117612ir9gw.png'),
+    Suggested(
+        brand: 'Subway. YSK',
+        price: '224',
+        title: 'Delight Veg Pizza',
+        image:
+            'http://www.pngpix.com/wp-content/uploads/2016/08/PNGPIX-COM-Pizza-PNG-Transparent-Image.png'),
+    Suggested(
+        brand: 'Starbucks. YSK',
+        price: '99',
+        title: 'Choco Latte',
+        image:
+            'https://www.starbucks.co.th/media/PeppermintMocha_tcm78-25963_w1024_n.png'),
+    Suggested(
+        brand: 'Flura. YSK',
+        price: '79',
+        title: 'White & Red Rose',
+        image:
+            'https://i.pinimg.com/originals/0b/6a/53/0b6a5364c1831a2ceb273110c6a11c35.png'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
+        color: Colors.white,
         image: DecorationImage(
             alignment: Alignment.topCenter,
             image: NetworkImage(
@@ -104,6 +134,65 @@ class ShopDetails extends StatelessWidget {
                       ),
                       Divider(
                         thickness: 2,
+                      ),
+                      Text(
+                        '40% OFF up to 200 on orders above 300 with Coupon',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 12,
+                                    bottom: 12,
+                                  ),
+                                  child: FittedBox(
+                                    child: Text(
+                                      'Suggested',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
+                                  ),
+                                ),
+                                GestureDetector(
+                                    onTap: () {},
+                                    child: FittedBox(
+                                      child: Text(
+                                        'View More >',
+                                        style: TextStyle(color: Colors.black38),
+                                      ),
+                                    ))
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GridView.builder(
+                                physics: ClampingScrollPhysics(),
+                                shrinkWrap: true,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  childAspectRatio: (1 / 1.1),
+                                ),
+                                itemBuilder: (context, index) {
+                                  return SuggestedItem(
+                                    brand: _images[index].brand,
+                                    image: _images[index].image,
+                                    price: _images[index].price,
+                                    title: _images[index].title,
+                                  );
+                                },
+                                itemCount: _images.length,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ],
                   ),
